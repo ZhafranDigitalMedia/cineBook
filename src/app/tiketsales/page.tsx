8456336367c2e20e3dc1b435faefff91c1d2ba4d
zhafran14 <80811273+ZhafranDigitalMedia@users.noreply.github.com>
2026-01-09 10:47:28 +0700

    tiket sales

---
+++ src/app/tiketsales/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Image from "next/image";
+import logo from "../../assets/cinebook-logo.png";
+import AdminHeaderNav from "../../components/adminHeaderNav";
+import TiketSales from "../../components/TiketSales";
+
+export default function HistoriPage() {
+  return (
+    <div style={{ background: "#6A77E0", minHeight: "100vh" }}>
+        <header
+        style={{
+          padding: "15px 25px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <Image
+          src={logo}
+          alt="CineBook logo"
+          width={100}
+          height={100}
+        />
+        <h1
+          style={{
+            fontSize: "40px",
+            fontWeight: 700,
+            color: "white",
+            margin: 0,
+          }}
+        >
+          CineBook
+        </h1>
+      </header>
+      <AdminHeaderNav />
+      <TiketSales />
+    </div>
+  );
+}
